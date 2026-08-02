5dbc6a12b65e8cde9e5d318b25d28c67ccff6d30
canabrava422-del <canabrava422@gmail.com>
2026-08-02 03:36:05 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "unidos-v46";
+const CACHE = "unidos-v48";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
